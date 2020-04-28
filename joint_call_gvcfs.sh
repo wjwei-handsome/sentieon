0525b1e2eb3783e2d21250851f0768fbcbc8ebd7
wjwei-handsome <56633839+wjwei-handsome@users.noreply.github.com>
2020-04-28 19:28:10 +0800

    combine

---
+++ joint_call_gvcfs.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+##load license
+export SENTIEON_LICENSE=mn01:9000
+SENTIEON_INSTALL_DIR=/public/home/software/opt/bio/software/Sentieon/201808.07
+
+UASGE='bash $(basename $0) -r your_ref_file -o output_file '
+
+## arguments setting
+while getopts ":r:o:" opt
+do
+    case $opt in
+        r)
+        REF=$OPTARG
+        ;;
+        o)
+        output_file=$OPTARG
+        ;;
+        ?)
+        echo ${USAGE}
+        exit 1;;
+    esac
+done
+
+##check arguments and file
+if [ ! $REF ];then
+    echo $UASGE;exit 
+fi
+if [ ! $output_file ];then
+    echo $UASGE;exit 
+fi
+if [ ! -f $REF ];then
+    echo "Error:Reference File $REF not exist ";exit
+fi
+## create a gvcf files list
+find ./ -name '*_scope.gvcf.gz' > gvcfs.list
+## 循环写变量
+gvcf_argument=''
+while read LINE;do
+    gvcf_argument=${gvcf_argument}" -v ${LINE}"
+done < gvcfs.list
+## remove file list
+rm gvcfs.list
+## joint call all gvcfs
+$SENTIEON_INSTALL_DIR/bin/sentieon driver -r ${REF} --algo GVCFtyper ${gvcf_argument} ${output_file}
